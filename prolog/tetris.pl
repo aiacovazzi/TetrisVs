2ee70a45a95a67a2b4279106b12273c40b460565
aiacovazzi <ar.iacovazzi@gmail.com>
2023-10-12 16:50:40 +0200

    fixed the heuristic, kickstart webserver

--- prolog/tetris.pl
+++ prolog/tetris.pl
@@ -1,10 +1,47 @@
 :- use_module(library(lists)).
 :- use_module(planner).
+:- use_module(library(http/http_server)).
+:- use_module(library(http/http_session)).
+
+%Web Server Interface Config
+/*
+Possible Action:
+-- startTetramino (assert the start tetramino)
+
+-- path (find the best path for the current tetramino)
+
+-- retractOccCell (reset all the occCell facts)
+
+
+...
+*/
+:- initialization
+    http_server([port(7777)]).
+
+:- http_handler(root(home), writeGB, []).
+
+%occCell/R/C (assert an occupied cell)
+:- http_handler(root(occCell/R/C), reqOccCell(R,C), []).
+
+:- http_handler(root(path/T), path(T), []).
+
+reqOccCell(R,C,_Request) :-
+    format('Content-type: text/plain~n~n'),
+    assert(occCell(R,C)),
+    writeGameBoard.
+
+path(T,_Request) :-
+    format('Content-type: text/plain~n~n'),
+    getPathOfBestMove(T,Plan),
+    write(Plan).
+
+writeGB(_Request) :-
+    format('Content-type: text/plain~n~n'),
+    writeGameBoard.
+%
 
 /*To do (8/10):
 -prima integrazione col gioco Js (TEST)
-
--l'euristica di selezione della mossa è da migliorare.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 To do (15/10):
@@ -15,29 +52,28 @@ To do (15/10):
 
 -implementare il logger che spieghi le mosse
     prende la catena di valutazione delle mosse e ne siega la ratio
+
+- algoritmo genetico per pesi euristica di valutazione??
 */
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %Defining the game board properties and the occupied cells%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 gameBoardW(10).
 gameBoardH(10).
+%occCell(R,C), assert this starting from the first row(0) to the last in asending order.
 :-dynamic(occCell/2).
+%the starting position of the playing tetramino
+%tetramino(T,R,C).
+:-dynamic(start/3).
 
 /*
-[-1,-1,-1,-1, 0, 0,-1,-1,-1,-1],
-[-1,-1,-1,-1, 0, 0,-1,-1,-1,-1],
-[-1,-1,-1, 0, 0, 0,-1,-1,-1,-1],
-[-1,-1,-1,-1, 0,-1,-1,-1,-1,-1],
-*/
-occCell(9,0).
-occCell(9,1).
-occCell(9,2).
-occCell(9,3).
-occCell(9,5).
-occCell(9,6).
-occCell(9,7).
-occCell(9,8).
-occCell(9,9).
+occCell(7,0).
+occCell(7,1).
+occCell(7,2).
+occCell(7,6).
+occCell(7,7).
+occCell(7,8).
+occCell(7,9).
 
 occCell(8,0).
 occCell(8,1).
@@ -47,28 +83,16 @@ occCell(8,7).
 occCell(8,8).
 occCell(8,9).
 
-occCell(7,0).
-occCell(7,1).
-occCell(7,2).
-occCell(7,3).
-occCell(7,6).
-occCell(7,7).
-occCell(7,8).
-occCell(7,9).
-
-occCell(6,0).
-occCell(6,1).
-occCell(6,2).
-occCell(6,3).
-occCell(6,6).
-occCell(6,7).
-occCell(6,8).
-occCell(6,9).
-
-
-%the starting position of the playing tetramino
-%tetramino(T,R,C).
-%start((T,1,5)).
+occCell(9,0).
+occCell(9,1).
+occCell(9,2).
+occCell(9,3).
+occCell(9,5).
+occCell(9,7).
+occCell(9,8).
+occCell(9,9).
+*/
+start((l1,1,5)).
 
 
 %%%%%%%%%%%%%%%%%
@@ -109,24 +133,7 @@ gen(Cur, Top, Next):-
   Cur < Top,
   Cur1 is Cur+1,
   gen(Cur1, Top, Next).
-%
-
-%Check if a cell is avalaible and if the indexes are not out of bound.
-freeCell(R,C) :- 
-    gameBoardH(H), 
-    gameBoardW(W), 
-    R >= 0, R<H, 
-    C >=0, C<W, 
-    \+occCell(R,C).
-
-%Generate all possible free cell on the gameboard
-freeCell1(R,C) :-     
-    gameBoardH(W),
-    gen(0, W, R),
-    gameBoardH(H),
-    gen(0, H, C),
-    \+occCell(R,C).
-    
+%    
 %/////////////////
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -154,6 +161,22 @@ rotation(j, j1, j2).
 rotation(j, j2, j3).
 rotation(j, j3, j4).
 rotation(j, j4, j1).
+
+%Check if a cell is avalaible and if the indexes are not out of bound.
+freeCell(R,C) :- 
+    gameBoardH(H), 
+    gameBoardW(W), 
+    R >= 0, R<H, 
+    C >=0, C<W, 
+    \+occCell(R,C).
+
+%Generate all possible free cell on the gameboard
+freeCell1(R,C) :-     
+    gameBoardH(W),
+    gen(0, W, R),
+    gameBoardH(H),
+    gen(0, H, C),
+    \+occCell(R,C).
 
 %Check if there is space for an a tetromino on the gameboard given the reference point.
 %R1;C1 is the reference point.
@@ -535,7 +558,7 @@ retractPiece([H|T]):-
     retract(H),
     retractPiece(T).
 
-%...
+%
 
 %Discover all the possible cell where a tetramino can be placed for all its rotations
 findPossibleGoals(T,List) :- 
@@ -549,7 +572,7 @@ findPossibleGoals0(Found,[Ht|T],L) :-
     append(Found,List,NewList),
     findPossibleGoals0(NewList,T,L).
 
-%a tetramino cam be placed on a free cell when the next row it collide
+%a tetramino can be placed on a free cell when the next row collide
 tetraminoGoal(T,R,C) :-
     freeCell1(R,C),
     fitPiece(T,R,C,_,_,_,_,_,_),
@@ -561,68 +584,46 @@ tetraminoGoal(T,R,C) :-
 %GameBoard Evaluator%
 %%%%%%%%%%%%%%%%%%%%%
 
-%1) Compute the number of holes in the gameboard.
+%1) Compute the number of holes in the columns.
 %This algorithm consider as holes all the empty cell below an occupied cell of a certain column.
-%The holes are often the result of a bad move, so the goal is to choose a mve that minimize this number.
-holesInGameBoard(N) :- 
-    holesInGameBoard0(0,0,N).
-
-holesInGameBoard0(C,N,N):-
-    gameBoardW(C).
-
-holesInGameBoard0(C,N,T):-
-    holesInColumn(0,C,n,0,Nh),
-    N1 is N + Nh,
-    C1 is C + 1,
-    holesInGameBoard0(C1,N1,T),
+%The holes are often the result of a bad move, so the goal is to choose a move that minimize this number.
+holesInColumn(N) :- 
+    setof((R,C),holesColumn(R,C),HolesColumn),
+    length(HolesColumn,N),
     !.
 
-holesInColumn(R,_,_,N,N) :- 
-    gameBoardH(R).
+holesInColumn(0).
 
-holesInColumn(R,C,n,N,NT) :- 
-    freeCell(R,C),
-    R1 is R + 1,
-    holesInColumn(R1,C,n,N,NT),
-    !.
-
-holesInColumn(R,C,y,N,NT) :- 
-    freeCell(R,C),
-    R1 is R + 1,
-    N1 is N + 1,
-    holesInColumn(R1,C,y,N1,NT),
-    !.
-
-holesInColumn(R,C,_,N,NT) :- 
+holesColumn(R1,C) :-
     occCell(R,C),
-    R1 is R + 1,
-    holesInColumn(R1,C,y,N,NT),
-    !.
+    freeCell1(R1,C),
+    R1 > R.
+
 %
 
-%2) Compute the etropy of each row, the give average value as measure of the "entropy" of the gameboard.
+%2) Compute the number of holes in the rows.
+%This algorithm consider as holes all the empty cell netx to an occupied cell of a certain row.
+%The holes are often the result of a bad move, so the goal is to choose a move that minimize this number.
+holesInRow(N) :- 
+    setof((R,C),holesRow(R,C),HolesRow),
+    length(HolesRow,N),
+    !.
 
-%Count the occupied cell of a row.
-countOccCelInRow(R,T) :- 
-    countOccCelInRow(R,0,0,T).
+holesInRow(0).
 
-countOccCelInRow(_,C,N,N) :-
-    gameBoardW(C).
-
-countOccCelInRow(R,C,N,T) :-
+holesRow(R,C1) :-
     occCell(R,C),
-    N1 is N+1,
-    C1 is C+1,
-    countOccCelInRow(R,C1,N1,T),
-    !.
+    freeCell1(R,C1),
+    C1 > C.
 
-%if the previous rule fails then is a free cell, don't need to check it
-countOccCelInRow(R,C,N,T) :-
-    C1 is C+1,
-    countOccCelInRow(R,C1,N,T),
-    !.
+%
 
-%Compute the entropy of a row and give as output also the numbero of occupied cells.
+%3) Compute the etropy of each row, then give average value as measure of the "entropy" of the gameboard.
+%Count the occupied cell of a row.
+countOccCelInRow(R,N) :- 
+    setof((C),occCell(R,C),Occ),length(Occ,N).
+
+%Compute the entropy of a row.
 entropyOfRow(R,Ent) :-
     countOccCelInRow(R,Occ),
     gameBoardW(W),
@@ -634,130 +635,40 @@ entropyOfRow(R,Ent) :-
     Ent is -OccRatio*Log2OccRatio -FreeRatio*Log2FreeRatio,
     !.
 
+entropyOfRow(R, 0).
+
 avgEntropyOfGameBoard(AvgEnt) :-
-    gameBoardH(H),
-    H1 is H-1,
-    avgEntropyOfGameBoard(H1,0,0,AvgEnt).
+    findall((Ent),entropyOfRow(_,Ent),EntropyXRow), 
+    sum_list(EntropyXRow,Sum),
+    length(EntropyXRow,L),
+    AvgEnt is Sum/L.
 
-%reached the top, stop
-avgEntropyOfGameBoard(-1,Acc,Div,AvgEnt) :-
-    AvgEnt is Acc/Div.
 
-%empty gameboard stop
-avgEntropyOfGameBoard(R,_,0,0):-
-    countOccCelInRow(R,Occ),
-    Occ = 0.
 
-%reached an empty row, stop
-avgEntropyOfGameBoard(R,Acc,Div,AvgEnt):-
-    countOccCelInRow(R,Occ),
-    Occ = 0,
-    AvgEnt is Acc/Div.    
-
-avgEntropyOfGameBoard(R,Acc,Div,AvgEnt):-
-    entropyOfRow(R,Ent),
-    Acc1 is Acc + Ent,
-    R1 is R - 1,
-    Div1 is Div + 1,
-    avgEntropyOfGameBoard(R1,Acc1,Div1,AvgEnt),
-    !.    
-
-%3) Compute the number of non-consecutive empty columns.
-%Each time the gamboard alternate an empty column to a full column (where full means at least one cell
-%occupied) a counter is increased, the total number is given as output only if it is >= than 2.
-nonConsecutiveEmptyCol(N) :-
-    emptyColumn(0),
-    nonConsecutiveEmptyCol0(1,e,0,N),
-    !.
-%if the preceeding rule does not match means that the column 0 was not empty...
-nonConsecutiveEmptyCol(N) :-
-    nonConsecutiveEmptyCol0(1,f,0,N),
-    !.
-
-nonConsecutiveEmptyCol0(C,_,N,N) :-
-    gameBoardW(C), 
-    N >= 2.
-
-nonConsecutiveEmptyCol0(C,_,_,0) :-
-    gameBoardW(C).
-
-nonConsecutiveEmptyCol0(C,e,N,T) :-
-    emptyColumn(C),
-    C1 is C+1,
-    nonConsecutiveEmptyCol0(C1,e,N,T),
-    !.
-
-%if the preceeding rule does not match means that the column was not empty...
-nonConsecutiveEmptyCol0(C,e,N,T) :-
-    C1 is C+1,
-    N1 is N+1,
-    nonConsecutiveEmptyCol0(C1,f,N1,T),
-    !.
-
-nonConsecutiveEmptyCol0(C,f,N,T) :-
-    \+emptyColumn(C),
-    C1 is C+1,
-    nonConsecutiveEmptyCol0(C1,f,N,T),
-    !.
-
-%if the preceeding rule does not match means that the column was empty...
-nonConsecutiveEmptyCol0(C,f,N,T) :-
-    C1 is C+1,
-    nonConsecutiveEmptyCol0(C1,e,N,T),
-    !.    
-
-emptyColumn(C) :-
-    emptyColumn(0,C,y).    
-
-emptyColumn(R,_,y) :-
-    gameBoardH(R).
-
-emptyColumn(R,C,E) :-
-    freeCell(R,C),
-    R1 is R + 1,
-    emptyColumn(R1,C,E),
-    !.
-
-emptyColumn(R,C,n) :-
-    occCell(R,C),
-    !.
 %
 
-%return the Row number of the cleared row
-clearedRow(C) :-
-    gameBoardH(H),
-    H1 is H - 1,
-    clearedRow(H1,[],C).
+%4) Return the number of the cleared row, this in order to give advantage to the move that actually clear some row.
 
-%reached the top, stop.
-clearedRow(-1,Ct,Ct).
-
-%reached the first empty row, stop.
-clearedRow(R,Ct,Ct) :-
-    countOccCelInRow(R,T),
-    T=0.
-
-clearedRow(R,Ct,C) :-
+clearedRow(R) :-
+    countOccCelInRow(R,Occ),
     gameBoardW(W),
-    countOccCelInRow(R,OccCell),
-    W=OccCell,
-    append(Ct,[R],Ct1),
-    R1 is R -1,
-    clearedRow(R1,Ct1,C),
+    Occ = W.
+
+numberOfclearedRow(N) :- 
+    setof((R),clearedRow(R),ClearedRow),
+    length(ClearedRow,N),
     !.
 
-%called if the previous rule fails
-clearedRow(R,Ct,C) :-
-    R1 is R -1,
-    clearedRow(R1,Ct,C),
-    !.
-    
+numberOfclearedRow(0).
+%
+
 %Compute the whole GameBoard score
 gameBoardScore(S) :-
-    holesInGameBoard(N),
+    holesInColumn(HC),
+    holesInRow(HR),
     avgEntropyOfGameBoard(AvgEnt),
-    nonConsecutiveEmptyCol(EC),
-    S is N + AvgEnt + EC.  
+    numberOfclearedRow(CR),
+    S is HC + HR + AvgEnt - CR.  
 
 scoreMoves([],M,M).
 
@@ -808,7 +719,7 @@ down((T1,R1,C1),(T1,R2,C1)) :-
     fitPiece(T1,R2,C1,_,_,_,_,_,_).
 %
 
-%evaluateMovement cmpute a score for each move given the goal, the score is weighted by the priority of the move.
+%evaluateMovement compute a score for each move given the goal, the score is weighted by the priority of the move.
 %the priority is: rotate, [left, right], down.
 %the ratio is: 
 %   a player first rotate a tetramino in the proper rotation
@@ -857,7 +768,7 @@ getPathOfBestMove(T,Plan) :-
     member(GoalNode, ScoreList),
     nth1(2, GoalNode, Goal), %take the goal 
     start(Start), 
-    write(Goal),
+    %write(Goal),
     serchPath(Goal, Start, RevPlan),
     reverse(RevPlan,Plan),
     !.
@@ -895,13 +806,13 @@ writeRow(_,C) :-
 
 writeRow(R,C) :- 
     occCell(R,C), 
-    write([o]), 
+    write([■]), 
     C1 is C + 1, 
     writeRow(R,C1),
     !.
 
 writeRow(R,C) :- 
-    write([x]),
+    write([□]),
     C1 is C + 1,
     writeRow(R,C1),
     !.
